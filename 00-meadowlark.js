10e6bae6b8ab1ee5cc0eb869a18785ebcd356955
Sami <67718664+Noxious-smr@users.noreply.github.com>
2022-11-30 15:06:13 +0100

    Add unit and integration tests using jest

--- 00-meadowlark.js
+++ 00-meadowlark.js
@@ -11,7 +11,8 @@ const port = process.env.port || 3000;
 //                 "You will have a pleasant surprise.",
 //                 "Whenever possible, keep it simple."
 //                 ];
-const fortune = require('./lib/fortune');
+// const fortune = require('./lib/fortune');
+const handlers = require('./lib/handlers')
 
 // configure handlebars view engine
 app.engine('handlebars', expressHandlebars.engine({
@@ -24,43 +25,56 @@ app.set('view engine', 'handlebars');
 
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', (req, res) => {
-    // res.type('text/plain');
-    // res.send('Meadowlark Travel');
-    res.render('home');
-});
+// app.get('/', (req, res) => {
+//     // res.type('text/plain');
+//     // res.send('Meadowlark Travel');
+//     res.render('home');
+// });
+app.get('/', handlers.home);
 
-app.get('/about', (req, res) => {
-    // res.type('text/plain');
-    // res.send('About Meadowlark Travel');
-    // const randomFortune = fortunes[Math.floor(Math.random()*fortunes.length)];
-    res.render('about', {fortune: fortune.getFortune()});
-    //res.render('about');
-});
+// app.get('/about', (req, res) => {
+//     // res.type('text/plain');
+//     // res.send('About Meadowlark Travel');
+//     // const randomFortune = fortunes[Math.floor(Math.random()*fortunes.length)];
+//     res.render('about', {fortune: fortune.getFortune()});
+//     //res.render('about');
+// });
+app.get('/about', handlers.about);
 
 
 // custom 404 page
-app.use((req, res) => {
-    // res.type('text/plain');
-    // res.status(404);
-    // res.send('404 - Not Found')
-    res.status(404);
-    res.render('404');
-});
+// app.use((req, res) => {
+//     // res.type('text/plain');
+//     // res.status(404);
+//     // res.send('404 - Not Found')
+//     res.status(404);
+//     res.render('404');
+// });
+app.use(handlers.notFound);
 
 
 // custom 500 page
-app.use((err, req, res, next) => {
-    console.error(err.message);
-    // res.type('text/plain');
-    // res.status(500);
-    // res.send('500 - Server Error');
+// app.use((err, req, res, next) => {
+//     console.error(err.message);
+//     // res.type('text/plain');
+//     // res.status(500);
+//     // res.send('500 - Server Error');
 
-    res.status(500);
-    res.render('500');
-});
+//     res.status(500);
+//     res.render('500');
+// });
+
+app.use(handlers.serverError);
 
 
-app.listen(port, () => console.log(
-    `Server is listening on port: ${process.env.PORT}\nExpress started on http://localhost:${port}; \npress Ctrl-C to terminate.`
-));
+// app.listen(port, () => console.log(
+//     `Server is listening on port: ${process.env.PORT}\nExpress started on http://localhost:${port}; \npress Ctrl-C to terminate.`
+// ));
+
+if(require.main === module){
+    app.listen(port, () => {
+        console.log(`Server is listening on port: ${process.env.PORT}\nExpress started on http://localhost:${port}; \npress Ctrl-C to terminate.`);
+    })
+} else{
+    module.exports = app;
+}
